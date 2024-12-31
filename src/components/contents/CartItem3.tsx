46d87926cd68f6c385ef4f6dda98b4ac903a4298
Adebimpe Adetomiwa <adebimpeadetomiwa22@gmail.com>
2024-12-31 13:53:57 -0800

    comment out CartItem3.tsx contents

--- src/components/contents/CartItem3.tsx
+++ src/components/contents/CartItem3.tsx
@@ -1,67 +1,67 @@
-const [books, setBooks] = useState<CartBookType[]>([]);
-const [book, setBook] = useState<CartBookType | null>(null);
+// const [books, setBooks] = useState<CartBookType[]>([]);
+// const [book, setBook] = useState<CartBookType | null>(null);
 
-// functions
-const handleClick = (action: 'increase' | 'decrease'): void => {
-    switch (action) {
-        case 'increase':
-            console.log(action);
-            if (book) {
-                setBook({ ...book, bookAmount: book.bookAmount + 1 });
-            }
-            break;
+// // functions
+// const handleClick = (action: 'increase' | 'decrease'): void => {
+//     switch (action) {
+//         case 'increase':
+//             console.log(action);
+//             if (book) {
+//                 setBook({ ...book, bookAmount: book.bookAmount + 1 });
+//             }
+//             break;
 
-        case 'decrease':
-            console.log(action);
-            if (book && book.bookAmount > 1) {
-                setBook({ ...book, bookAmount: book.bookAmount - 1 });
-            }
-            break;
-        default:
-            console.log(`Unknow action: ${action}`);
-    }
-};
+//         case 'decrease':
+//             console.log(action);
+//             if (book && book.bookAmount > 1) {
+//                 setBook({ ...book, bookAmount: book.bookAmount - 1 });
+//             }
+//             break;
+//         default:
+//             console.log(`Unknow action: ${action}`);
+//     }
+// };
 
-const fetchBooks = (): void => {
-    const booksString: string | null = localStorage.getItem('books');
-    if (booksString) {
-        setBooks(JSON.parse(booksString));
-    }
-};
+// const fetchBooks = (): void => {
+//     const booksString: string | null = localStorage.getItem('books');
+//     if (booksString) {
+//         setBooks(JSON.parse(booksString));
+//     }
+// };
 
-const fetchBook = (): void => {
-    if (books) {
-        const bookData = books.find((item) => item.bookID === bookID);
-        if (bookData) {
-            setBook(bookData);
-        }
-    }
-};
+// const fetchBook = (): void => {
+//     if (books) {
+//         const bookData = books.find((item) => item.bookID === bookID);
+//         if (bookData) {
+//             setBook(bookData);
+//         }
+//     }
+// };
 
-const updateBook = (): void => {
-    if (books && book) {
-        const newBook: CartBookType[] = books.map((item) =>
-            item.bookID === book.bookID ? book : item
-        );
-        localStorage.setItem('books', JSON.stringify(newBook));
-        console.log(newBook);
-    }
-};
+// const updateBook = (): void => {
+//     if (books && book) {
+//         const newBook: CartBookType[] = books.map((item) =>
+//             item.bookID === book.bookID ? book : item
+//         );
+//         localStorage.setItem('books', JSON.stringify(newBook));
+//         console.log(newBook);
+//     }
+// };
 
-// use effects
+// // use effects
 
-useEffect(() => {
-    fetchBooks();
-}, [bookID]);
+// useEffect(() => {
+//     fetchBooks();
+// }, [bookID]);
 
-useEffect(() => {
-    fetchBook();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [books]);
+// useEffect(() => {
+//     fetchBook();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+// }, [books]);
 
-useEffect(() => {
-    if (book) {
-        updateBook();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [book]);
+// useEffect(() => {
+//     if (book) {
+//         updateBook();
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+// }, [book]);
