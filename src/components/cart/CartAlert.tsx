2c22e41b01063be533afa9829419784a04f45cb2
Adebimpe Adetomiwa <adebimpeadetomiwa22@gmail.com>
2025-01-09 11:43:01 -0800

    change card alert to easy mode

--- src/components/cart/CartAlert.tsx
+++ src/components/cart/CartAlert.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react';
-import { useStore } from '../../store';
-import { Book } from '../../types';
+// import { useEffect, useState } from 'react';
+// import { useStore } from '../../store';
+// import { Book } from '../../types';
 
 const CartAlert = () => {
-    const [book, setbook] = useState<Book | null>(null);
-    const books: Book[] = useStore((store) => store.books);
-    const addingBook = useStore((store) => store.cart.addingBook);
+    // const [book, setbook] = useState<Book | null>(null);
+    // const books: Book[] = useStore((store) => store.books);
+    // const addingBook = useStore((store) => store.cart.addingBook);
 
-    useEffect(() => {
-        if (addingBook) {
-            const fetchedBook = books.find((book) => book.id === addingBook);
-            setbook(fetchedBook as Book);
-        }
-    }, [addingBook, books]);
+    // useEffect(() => {
+    //     if (addingBook) {
+    //         const fetchedBook = books.find((book) => book.id === addingBook);
+    //         setbook(fetchedBook as Book);
+    //     }
+    // }, [addingBook, books]);
+    // console.log(addingBook);
     return (
         <div className='w-screen bg-secondary h-12 fixed left-0 top-0 z-10'>
             <div className='container w-full'>
                 <div className='text-sm w-full'>
-                    <span className='font-semibold'>
-                        {book?.title || 'no-book-yet'}
-                    </span>{' '}
-                    added to cart
+                    <span className=''>{'Book'}</span> added to cart
                 </div>
             </div>
         </div>
